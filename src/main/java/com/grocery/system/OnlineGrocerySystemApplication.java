582956f2ad57c3f34926f00d5f17b67b9a45cbf3
ThenulaD <thenulive@outlook.com>
2025-04-06 18:31:08 +0530

    Just plain springboot files

---
+++ src/main/java/com/grocery/system/OnlineGrocerySystemApplication.java
@@ -0,0 +1,13 @@
+package com.grocery.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGrocerySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineGrocerySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grocery/system/OnlineGrocerySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocery.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGrocerySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
